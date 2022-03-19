b3bfea8ed12ba41f47e523165ebdd7dfc57f3397
jianyi <934384533@qq.com>
2022-03-20 04:09:27 +0800

    feat: init

---
+++ main.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import App from './src/index';
+
+if((module as any).hot){
+    (module as any).hot.accept(error=>{
+        if(error){
+            console.log("热替换出现bug", error);
+        }else{
+            console.log('hot update success')
+        }
+    })
+}
+
+ReactDom.render(<App />, document.getElementById('app'))
+
+
+
---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
+const webpack = require('webpack')
+
+module.exports = {
+    mode: 'development',
+    target: 'web',
+    resolve: {
+        extensions: ['.js', '.ts', '.tsx'],
+    },
+    devtool: 'inline-source-map',
+    entry: path.resolve(__dirname, './main.tsx'),  //工程资源入口
+    output: {
+        filename: 'main.js', // 打包过后的文件名
+        path: path.join(__dirname, './dist') //打包后的目录
+    },
+    devServer: {
+        hot: true,
+        port: 3001,
+        open: true,
+        // liveReload: false,
+        // watch: true,
+        static: {
+            directory: path.join(__dirname, 'dist')
+        },
+        // inline: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: 'babel-loader'
+                        // loader: 'ts-loader',
+                        // options:{
+                        //     transpileOnly: true  
+                        // }
+                    }
+                ],
+            },
+            {
+                test: /\.ts?$/,
+                exclude: /node_modules/,
+                use: [{
+                    loader: 'babel-loader'
+                }]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'webpack demo',
+            template: path.resolve(__dirname, 'index.html'),
+            filename: 'index.html'
+        }),
+        new webpack.HotModuleReplacementPlugin()
+    ]
+
+}
