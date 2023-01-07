440140693c8e0e3a4912da8435ad759fec0d0f6b
jianyi <934384533@qq.com>
2023-01-08 00:44:53 +0800

    feat: add method persist redux store

--- src/redux/store/index.ts
+++ src/redux/store/index.ts
@@ -1,5 +1,10 @@
-import { createStore  } from 'redux'
+import { createStore } from 'redux'
 // import { configureStore } from '@reduxjs/toolkit'
+import { reducer } from '../reducer'
+// import { configureStore } from '@reduxjs/toolkit'
+import { persistStore, persistReducer  } from 'redux-persist'
 
+// const 
 
 export const store = createStore(reducer)
+
